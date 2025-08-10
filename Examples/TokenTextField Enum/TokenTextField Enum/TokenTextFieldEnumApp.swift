c4379ed4ff5238e87a66173ec53d190c917d9695
Steffan Andrews <stef@orchetect.com>
2025-08-09 21:41:11 -0700

    Renamed 'TokenTextField Demo' example project to 'TokenTextField Enum'

--- Examples/TokenTextField Enum/TokenTextField Enum/TokenTextFieldEnumApp.swift
+++ Examples/TokenTextField Enum/TokenTextField Enum/TokenTextFieldEnumApp.swift
@@ -1,5 +1,5 @@
 //
-//  TokenTextFieldDemoApp.swift
+//  TokenTextFieldEnumApp.swift
 //  SwiftUITokenField • https://github.com/orchetect/SwiftUITokenField
 //  © 2025 Steffan Andrews • Licensed under MIT License
 //
@@ -7,7 +7,7 @@
 import SwiftUI
 
 @main
-struct TokenTextFieldDemoApp: App {
+struct TokenTextFieldEnumApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
